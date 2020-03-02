9ec1634056e1132067cb416582bd1732b4ec6479
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-03-01 21:55:46 -0600

    Trying to use json

--- src/data.js
+++ src/data.js
@@ -4,21 +4,43 @@
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
- import data from './data/worldbank/worldbank.js';
+// import data from './data/worldbank/worldbank.js';
  
 // esta es una función de ejemplo
 
 export const example = () => {
-  let datoPrueba;
-  let datos;
+
+
+  //let datoPrueba;
+  //let datos;
   //datoPrueba = Object.values(data.PER.indicators[1].indicatorName);
-  
+  /*
   for(let i in Object.values(data.PER.indicators)){
     datoPrueba = Object.values(data.PER.indicators[i].indicatorName);
     datos += i+".-"+ datoPrueba.join("") + ".\n";
   }
   
-  return datos;
+  return datos;*/
   //return datoPrueba.length;
+  //return data;
+  
 };
+
+
+
+
+
+/*fetch('./data/worldbank/worldbank.json')
+   
+  .then(response => {
+    let data = await response.json();
+    example(data);
+  })
+  .then(data => {
+    // Work with JSON data here
+    //console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  })*/
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import { example } from './data.js';
 
 console.log(example());
 
+fetch('./data/worldbank/worldbank.json')
+  .then(function(resp){
+  return resp.json();
+})
+let algoThen =  p.then(function(data){
+    return data;
+})
+
+
+console.log(algoThen);
+
+
+/*
 document.getElementById("rita").addEventListener("click", on);
 document.getElementById("overlay").addEventListener("click", off);
 
@@ -13,3 +26,5 @@ function on() {
   function off() {
     document.getElementById("overlay").style.display = "none";
   }
+*/  
+
