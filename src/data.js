ff5d98ba7ef8878c21490e823b70386c0511f424
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-03-05 13:39:20 -0600

    frustration tryin to filter indicators... includes doesn't works as I thought

--- src/data.js
+++ src/data.js
@@ -6,27 +6,88 @@
 // import data from './data/steam/steam.js';
 import data from './data/worldbank/worldbank.js';
  
-// esta es una función de ejemplo
+/* data para worldbank es un objeto. Cuando se trae con Object.entries
+regresa como un array, sin embargo, es más "sencillo" traer los datos
+a través de keys y values para el primer nivel. Así, solo tomamos los datos
+que el usuario a elegido. 
+Cada que se obtienen datos desde Object.método(nombre_objeto), regresa
+como un array.
+*/
 
 export const example = () => {
   
   let bankData = Object.entries(data);
   let country = "MEX";
+  let category = "fuerza laboral"; 
   let countryData;
+  let countryIndicators;
+  let countryIndicator;
+  let categoryKeyWords = {"fuerzaLaboral":["fuerza", "laboral", "activa", "activo", 
+                                          "negocio", "empresas", "capital humano", 
+                                          "trabajo", "empleados", "trabajador",
+                                          "estimación"],
+                       "poblacionMujeres":["población", "mujeres"],
+                              "desempleo":["desempleo", "desempleados", "desempleadas"],
+                              "educacion":["alumnos", "escuela", "escolar", "ingreso", "inscripción",
+                                          "alfabetización", "alfabetizados"]};
+  
 
-  /*
   for(let i=0; i <= Object.keys(data).length; i ++){
     if(Object.keys(data)[i] == country){
       countryData = Object.values(data)[i];
+      //countryData es un objeto
     }
+  }
+
+  /*  
+  const getIndicatorsName = () => {
+    //return countryIndicators[0];
+    return countryIndicators.filter(element => {
+      return element.indicatorName.includes(category) || 
+             element.indicatorName.includes("activa") ||
+             element.indicatorName.includes("activo") ||
+             element.indicatorName.includes("negocio") ||
+             element.indicatorName.includes("empresas") ||
+             element.indicatorName.includes("Capital Humano")})
   }*/
 
-  //const array1 = ['a', 'b', 'c'];
-  let algo = Object.entries(data).filter(element => element[0] == country);
-  countryData = Object.keys(algo[0][1]);
-  //let indicador = algo.filter(element => element[1] == "indicators");
+  /*
+  const filterKeyWords = () => {
+    categoryKeyWords.filter(e => {})
+  }*/
+  
+  
+  const getIndicatorsName = () => {
+    //return countryIndicators[0];
+    return countryIndicators.filter(element => {
+      //let keyW = Object.values(categoryKeyWords.fuerzaLaboral);
+      return element.indicatorName.includes("fuerza" || "laboral");
+    });
+  }
 
-  return countryData;
+  function prueba(e){
+    return true;
+  }
+
+  for(let i=0; i <= Object.keys(countryData).length; i ++){
+    if(Object.keys(countryData)[i] == "indicators"){
+      countryIndicators = Object.values(countryData)[i];
+      //getIndicatorsName(countryIndicators);
+    }
+  }
+  
+  //countryIndicators.forEach(element => countryIndicator += element);
+
+  /*
+  for(let i=0; i <= countryIndicators.length; i ++){
+    countryIndicator = countryIndicators[i];
+  }*/
+  
+  //let algo = countryIndicators.filter(element =>  == "indicatorName");
+  
+  //let indicador = algo.filter(element => element[1] == "indicators");
+  
+  return getIndicatorsName();
 
 };
 
