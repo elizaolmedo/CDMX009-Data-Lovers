4eb7e110cdbe9c2a3b404c62da6d27447c8fd367
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-03-13 17:47:43 -0600

    Bars & Dots almost complete

--- src/indicators.js
+++ src/indicators.js
@@ -1,23 +1,20 @@
-//import * from './main.js'; 
 import  bankData  from './data.js';
 
-
-let country = "PER";
-let category = "población";
-//let indicator = "Inscripción escolar, nivel terciario, mujeres (% bruto)";
+let country = localStorage.country;
+let category = localStorage.category;
 let indicator;
 let indicatorData;
-let cardId;
+
+
+let indicatorName = bankData.getIndicatorsName(country, category);
 let section =  document.getElementById("indicatorsCards");
 let indicatorsCards = document.getElementsByClassName("aCard");
 let indicatorSelected = document.getElementsByClassName("indicatorTittle");
-let indicatorName = bankData.getIndicatorsName(country, category);
-let overlayDiv = document.getElementById("text");
 let pageTittle = document.getElementById("pageTittle");
 let modal = document.getElementById("myModal");
 let span = document.getElementsByClassName("close")[0];
 
-pageTittle.textContent = country.toUpperCase() + "-" + category.toUpperCase();
+pageTittle.textContent = country.toUpperCase() + " - " + category.toUpperCase();
 
 for (let i=0; i< indicatorName.length; i ++){
     let card = document.createElement("div");
@@ -26,8 +23,6 @@ for (let i=0; i< indicatorName.length; i ++){
     let figure = document.createElement("figure");
     let img = document.createElement("img");
     let cardContent = document.createElement("div");
-   // let media = document.createElement("div");
-   // let tittle = document.createElement("p");
     let content = document.createElement("div");
     let indicatorTittle = document.createElement("p");
     let footer = document.createElement("footer");
@@ -42,8 +37,6 @@ for (let i=0; i< indicatorName.length; i ++){
     cardImage.setAttribute("class","card-image");
     figure.setAttribute("class","imageMedia");
     cardContent.setAttribute("class","card-content");
-   // media.setAttribute("class","media");
-   // tittle.setAttribute("class","tittle is-4");
     content.setAttribute("class","content");
     indicatorTittle.setAttribute("class","indicatorTittle");
     footer.setAttribute("class","share-btn");
@@ -56,9 +49,7 @@ for (let i=0; i< indicatorName.length; i ++){
     aTwitter.setAttribute("href","https://twitter.com/share?url=<URL>&text=<TEXT>via=<USERNAME>");
     aFacebook.setAttribute("href","https://www.facebook.com/sharer/sharer.php?u=<URL>");
     aMail.setAttribute("href","mailto:?subject=<SUBJECT>&body=<BODY>");
-    //aCard.setAttribute("href","");
-    
-  //  tittle.innerHTML= category.toUpperCase();
+        
     indicatorTittle.innerHTML= indicatorName[i] + "." ;
     aTwitter.innerHTML= "Twitter";
     aFacebook.innerHTML= "Facebook";
@@ -66,8 +57,6 @@ for (let i=0; i< indicatorName.length; i ++){
     
     figure.appendChild(img);
     cardImage.appendChild(figure);
-  //  media.appendChild(tittle);
-  //  cardContent.appendChild(media);
     cardContent.appendChild(content);
     footer.appendChild(aTwitter);
     footer.appendChild(aFacebook);
@@ -80,126 +69,80 @@ for (let i=0; i< indicatorName.length; i ++){
     section.appendChild(card);
 }
 
-
-const showGraphic = (cardId,indicator) => {
-
-  modal.style.display = "block";
-  let modalHeader = document.getElementById("modalHeader");
-  modalHeader.textContent = category.toUpperCase();
-  let modalIndicator = document.getElementById("modalIndicator");
-  modalIndicator.textContent = indicator;
+const createGraphic = () => {
   let graphicModal = document.getElementById("graphicModal");
-
   let graphic =  document.createElement("canvas");
   graphic.setAttribute("id","myChart");
   graphicModal.appendChild(graphic);
+}
 
-  indicatorData = bankData.getIndicatorsData(country, category, indicator);
-  console.log(Object.keys(indicatorData));
-  console.log(Object.values(indicatorData));
-  console.log(bankData.getIndicatorsData(country, category, indicator));
-  //overlayDiv.innerHTML = Object.keys(indicatorData);
-  /*
-  let barChartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'Dataset 1',
-      backgroundColor: 'rgba(242,193,19,0.5)',
-      data: [
-        10, 20, 30, 40, 50
-      ]
-    }, {
-      label: 'Dataset 2',
-      backgroundColor: 'rgb(66, 134, 244,1)',
-      data: [
-        5, 15, 25, 30, 35, 40
-      ]
-    }, {
-      label: 'Dataset 3',
-      backgroundColor: 'rgb(229, 89, 50,1)',
-      data: [
-        20, 23, 34, 45, 56
-      ]
-    }]
-
-  };*/
-
-
-
+const compareGraphic = () => {
   var ctx= document.getElementById("myChart").getContext("2d");
   var myChart= new Chart(ctx,{
       type:"line",
       data:{
           labels:Object.keys(indicatorData),
           datasets:[{
-                    label:country, 
-                    data:Object.values(indicatorData),
-                    //data: barChartData,
-                    backgroundColor:'rgba(242,193,19,0.5)',
-                    borderColor: 'rgba(242,193,19,1)',
+                    label:'PER', 
+                    data:Object.values(bankData.getIndicatorsData('PER', category, indicator)),
+                    backgroundColor:'rgba(250,150,0,0.5)',
+                    borderColor: 'rgba(250,150,0,1)',
                     borderWidth: 3, 
                     fill: false
-                   },{
+                  },{
                     label:'BRA', 
                     type: 'line',
                     data:Object.values(bankData.getIndicatorsData('BRA', category, indicator)),
-                    //data: barChartData,
                     backgroundColor:'rgb(79, 205, 169,0.5)',
                     borderColor: 'rgb(79, 205, 169,1)',
                     borderWidth: 3, 
                     fill: false                 
-                   },{
+                  },{
                     label:'MEX', 
                     type: 'line',
                     data:Object.values(bankData.getIndicatorsData('MEX', category, indicator)),
-                    //data: barChartData,
                     backgroundColor:'rgb(229, 89, 50,1)',
                     borderColor: 'rgb(229, 89, 50,1)',
                     borderWidth: 3, 
                     fill: false                 
-                   },{
+                  },{
                     label:'CHL', 
                     type: 'line',
-                    data:Object.values(bankData.getIndicatorsData('CHL', category, indicator)),
-                    //data: barChartData,
+                    data: Object.values(bankData.getIndicatorsData('CHL', category, indicator)),
                     backgroundColor:'rgb(66, 134, 244,1)',
                     borderColor: 'rgb(66, 134, 244,1)',
                     borderWidth: 3, 
                     fill: false                 
-                  } /*{
-                  label: '',
-                  data:[10, 50, 60],
-                  type:'bar',
-                  backgroundColor:'rgb(74, 135, 72,1)',
-                
-                  }, {
-                  label: '',
-                  data:[10, 40, 50],
-                  type:'bar',
-                  backgroundColor:'rgb(229, 89, 50,1)',
-                
-                }*/
-               ]},
+                  } 
+              ]},
+                options:{
+                  scales:{
+                        yAxes:[{
+                                ticks:{
+                                    beginAtZero:true
+                                }
+                        }]
+                    }
+                }
+            });
+}
+
+const oneGraphic = () => {
+  var ctx= document.getElementById("myChart").getContext("2d");
+  var myChart= new Chart(ctx,{
+      type:"bar",
+      data:{
+          labels:Object.keys(indicatorData),
+          datasets:[{
+                    label:country, 
+                    data:Object.values(indicatorData),
+                    backgroundColor:'rgba(242,193,19,0.5)',
+                    borderColor: 'rgba(242,193,19,1)',
+                    borderWidth: 3 
+                    
+                  }
+              ]},
       options:{
-          /*
-					title: {
-						display: true,
-						text: indicator
-					},
-					tooltips: {
-						mode: 'index',
-						intersect: false
-					},
-					responsive: true,
-					scales: {
-						xAxes: [{
-							stacked: true,
-						}],
-						yAxes: [{
-							stacked: true
-						}]
-					}*/
-          
         scales:{
               yAxes:[{
                       ticks:{
@@ -209,46 +152,67 @@ const showGraphic = (cardId,indicator) => {
           }
       }
   });
-} 
-
-
-for(let i=0; i < indicatorsCards.length; i ++){
-    indicatorsCards[i].addEventListener("click", indicatorsSelected => {
-    cardId = indicatorSelected[i].id;
-    indicator = indicatorSelected[i].textContent.slice(0,-1);
-    showGraphic(cardId,indicator);
-    //console.log(indicator);
-    //console.log(indicatorData);
-    //return indicator;
-  });
 }
 
 
+const showGraphic = (indicator) => {
+  let toggleButtonFlag = false;
+  let modalHeader = document.getElementById("modalHeader");
+  let modalIndicator = document.getElementById("modalIndicator");
+  let compareButton = document.getElementById("compare");
+  
+  createGraphic();
 
-// Get the button that opens the modal
-//var btn = document.getElementById(cardId);
+  modal.style.display = "block";
+  modalHeader.textContent = category.toUpperCase();
+  modalIndicator.textContent = indicator;
+  indicatorData = bankData.getIndicatorsData(country, category, indicator);
+  
+  console.log(Object.keys(indicatorData));
+  console.log(Object.values(indicatorData));
+  console.log(bankData.getIndicatorsData(country, category, indicator));
 
-// Get the <span> element that closes the modal
+  oneGraphic();
 
-// When the user clicks the button, open the modal 
-// btn.onclick = function() {
-// modal.style.display = "block";
-// }
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+  compareButton.addEventListener("click", compared => {
+    removeGraphic();
+    createGraphic();
+    if (toggleButtonFlag == false){
+      compareButton.value = "Regresar";
+      compareGraphic();   
+      toggleButtonFlag = true;
+    }else{
+      compareButton.value = "Comparar";
+      oneGraphic();        
+      toggleButtonFlag = false;
+    }
+  });
+} 
+
+for(let i=0; i < indicatorsCards.length; i ++){
+    indicatorsCards[i].addEventListener("click", indicatorsSelected => {
+    indicator = indicatorSelected[i].textContent.slice(0,-1);
+    showGraphic(indicator);
+  });
+}
+
+const removeGraphic = () => {
   let graphicModal = document.getElementById("graphicModal");
   let eraseChild = graphicModal.lastChild;
-  graphicModal.removeChild(eraseChild); 
+  graphicModal.removeChild(eraseChild);
+} 
+
+// When the user clicks (x) of the modal, close it
+span.onclick = function() {
+  modal.style.display = "none";
+  removeGraphic();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    let graphicModal = document.getElementById("graphicModal");
-    let eraseChild = graphicModal.lastChild;
-    graphicModal.removeChild(eraseChild);
+    removeGraphic();
   }
 }
 
